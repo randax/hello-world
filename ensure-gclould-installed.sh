7f5d9df454000d66744967630ae28c71b9d2e5b9
Oyvind Randa <st08935@student.uib.no>
2016-04-06 00:22:40 +0200

    Dockerfile + circle.yml

---
+++ ensure-gclould-installed.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+pip install pyopenssl
+if [ ! -d ~/google-cloud-sdk ]; then
+    curl https://sdk.cloud.google.com | bash;
+    ~/google-cloud-sdk/bin/gcloud components update preview
+fi
